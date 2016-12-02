6f976dd2d660dcbdb2c7526487208e1dba9d8685
Antonio <eltony162@gmail.com>
2016-12-02 12:50:35 -0600

    Angular JS With ASP.NET MVC

---
+++ AngularJSWithASPNETMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AngularJSWithASPNETMVC.Models;
+using System.Web.Mvc;
+
+namespace AngularJSWithASPNETMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var employee = new Employee() { FirstName = "Antonio", LastName = "Acosta" };
+            return View(employee);
+        }
+
+
+        public ActionResult UpdateDate(Employee param)
+        {
+            var emp = new Employee() { FirstName = "Ing. Antonio", LastName = "Acosta Murillo", Input = param.Input + " -- Sucessful Update --"};
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AngularJSWithASPNETMVC/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace AngularJSWithASPNETMVC.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Input = string.Empty;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Input { get; set; }
+    }
+}
